e2c8fedf6adaef86908b5b6498c57ff9b95d816a
Yana D. Nugraha <me@djzmo.com>
2023-08-06 20:07:09 +0900

    Catch API errors & add winston logger

--- packages/api/src/app.ts
+++ packages/api/src/app.ts
@@ -1,12 +1,24 @@
 import express from "express"
 import cors from "cors"
 import { Client } from "redis-om"
+import winston from "winston";
 import StoreRepository from "./repository/StoreRepository"
 import StoreService from "./service/StoreService"
-;(async () => {
+
+(async () => {
   const client = await new Client().open(
     process.env.REDIS_URL || "redis://redis:6379"
   )
+  const logger = winston.createLogger({
+    level: process.env.LOG_LEVEL || "debug",
+    format: winston.format.combine(
+        winston.format.timestamp(),
+        winston.format.json()
+    ),
+    transports: [
+        new winston.transports.Console()
+    ]
+  })
   await client.execute(["FLUSHALL"])
   const storeRepository = new StoreRepository(client)
   const storeService = new StoreService(storeRepository)
@@ -18,25 +30,37 @@ import StoreService from "./service/StoreService"
   app.use(cors())
 
   app.get("/store/searchByText", async (req, res) => {
-    const { query } = req.query
-    if (typeof query !== "string" || query.length < 2) {
-      res.json([])
-    } else {
-      const result = await storeService.searchByQueryString(query)
-      res.json(result)
+    try {
+      const { query } = req.query
+      if (typeof query !== "string" || query.length < 2) {
+        await res.json([])
+      } else {
+        const result = await storeService.searchByQueryString(query.trim())
+        await res.json(result)
+      }
+    } catch (e) {
+      const meta = e instanceof Error ? [e.name, e.message, e.stack] : [e]
+      logger.error("Error during searchByText", meta)
+      await res.status(500).json([])
     }
   })
 
   app.get("/store/searchByPosition", async (req, res) => {
-    const { lat, lng } = req.query
-    if (Number.isNaN(lat) || Number.isNaN(lng)) {
-      res.json([])
-    } else {
-      const result = await storeService.searchByPosition(
-        parseFloat(lat as string),
-        parseFloat(lng as string)
-      )
-      res.json(result)
+    try {
+      const { lat, lng } = req.query
+      if (Number.isNaN(lat) || Number.isNaN(lng)) {
+        await res.json([])
+      } else {
+        const result = await storeService.searchByPosition(
+            parseFloat(lat as string),
+            parseFloat(lng as string)
+        )
+        await res.json(result)
+      }
+    } catch (e) {
+      const meta = e instanceof Error ? [e.name, e.message, e.stack] : [e]
+      logger.error("Error during searchByPosition", meta)
+      await res.status(500).json([])
     }
   })
 
